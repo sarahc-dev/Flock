f5364d17fc5623792ca8ef4df0f6336ef0ef0aec
Evan Thomas <evanthomas0292@gmail.com>
2023-07-05 11:27:30 +0100

    In progress

--- frontend/app/index.js
+++ frontend/app/index.js
@@ -1,15 +1,32 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View, SafeAreaView } from "react-native";
-import FlashCardContainer from "../components/FlashCardContainer"
+import FlashCardContainer from "../components/FlashCardContainer";
+import { useState } from "react";
+
 export default function Home() {
-  
+  const [user1Choices, setUser1Choices] = useState([]);
+  const [user2Choices, setUser2Choices] = useState([]);
+  const activities = [
+    "go for a walk",
+    "eat pizza",
+    "dance party",
+    "have a conversation",
+    "base jumping",
+  ];
+
+  const addUser1Choice = (choice) => {
+    setUser1Choices(user1Choices + choice);
+  };
+
+  const addUser2Choice = (choice) => {
+    setUser2Choices(user2Choices + choice);
+  };
 
   return (
     <View style={styles.container}>
-        <FlashCardContainer/>
+      <FlashCardContainer />
       <StatusBar style="auto" />
     </View>
-
   );
 }
 

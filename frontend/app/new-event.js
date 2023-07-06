3dc4eba801b244ebe27253b4ccbcdf8972f40cb9
sarahc-dev <sarahclements@hotmail.co.uk>
2023-07-06 15:36:46 +0100

    add fetch request to add event

--- frontend/app/new-event.js
+++ frontend/app/new-event.js
@@ -1,70 +1,86 @@
 import { useState, useEffect } from "react";
-import { View, Text, TextInput, SafeAreaView, TouchableOpacity } from "react-native"
-
+import { View, Text, TextInput, SafeAreaView, TouchableOpacity } from "react-native";
+import { Link } from "expo-router";
 
 export default function NewEvent() {
+    const [name, setName] = useState("");
+    const [nameList, setNameList] = useState([]);
+    const [eventName, setEventName] = useState("");
 
-    const [name, setName] = useState('');
-    const [nameList, setNameList] = useState([])
-    const [eventName, setEventName] = useState('')
-
-    const nameInput = (text) => {
+    const nameInput = text => {
         setName(text);
     };
 
     const addName = () => {
         setNameList([...nameList, name]);
-        setName('');
-    }
+        setName("");
+    };
 
-    const removeName = (nameToRemove) => {
+    const removeName = nameToRemove => {
         const filteredNames = nameList.filter(name => name !== nameToRemove);
-        setNameList(filteredNames)
-        console.log('remove names called')
-        console.log(nameToRemove)
-    }
+        setNameList(filteredNames);
+        console.log("remove names called");
+        console.log(nameToRemove);
+    };
 
-    const eventNameInput = (text) => {
+    const eventNameInput = text => {
         setEventName(text);
-    }
+    };
+
+    const submitEvent = () => {
+        fetch("https://localhost:1066/event", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ eventName: eventName, names: nameList }),
+        })
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                // return "id"
+            })
+            .catch(error => {
+                console.error(error);
+            });
+    };
 
     useEffect(() => {
-        console.log(nameList)
-    }, [nameList]) 
-
-
+        console.log(nameList);
+    }, [nameList]);
 
     return (
         <SafeAreaView>
-          <View>
-            <Text>Create Event</Text>
-            <TextInput
-              placeholder="Enter event name"
-              value={eventName}
-              onChangeText={eventNameInput}
-            />
-            <TextInput
-              placeholder="Enter name and press enter..."
-              value={name}
-              onChangeText={nameInput}
-              onSubmitEditing={() => {
-                addName();
-
-              }}
-            />
-          </View>
-          <View>
-            {nameList.map((name) => (
-                <View key={name}>
-                <Text >{name}</Text>
-                <TouchableOpacity onPress={() => removeName(name)}>
-                    <Text>X</Text>
+            <View>
+                <Text>Create Event</Text>
+                <TextInput placeholder="Enter event name" value={eventName} onChangeText={eventNameInput} />
+                <TextInput
+                    placeholder="Enter name and press enter..."
+                    value={name}
+                    onChangeText={nameInput}
+                    onSubmitEditing={() => {
+                        addName();
+                    }}
+                />
+            </View>
+            <View>
+                {nameList.map(name => (
+                    <View key={name}>
+                        <Text>{name}</Text>
+                        <TouchableOpacity onPress={() => removeName(name)}>
+                            <Text>X</Text>
+                        </TouchableOpacity>
+                    </View>
+                ))}
+            </View>
+            <View>
+                <TouchableOpacity onPress={submitEvent}>
+                    <Text>Generate Link</Text>
                 </TouchableOpacity>
-                </View>
-            ) )}
-          </View>
-
+            </View>
+            <View>
+                <Link href="/">Choose Activities (next page)</Link>
+            </View>
         </SafeAreaView>
-        
-    )
+    );
 }

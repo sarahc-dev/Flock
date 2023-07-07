b6ae3eb5eb532dd2e613b94e7b8c5ed942dd1e4f
Evan Thomas <evanthomas0292@gmail.com>
2023-07-07 14:49:26 +0100

    Add openai client

---
+++ openAiClient.js
@@ -0,0 +1,41 @@
+require("dotenv").config();
+const { Configuration, OpenAIApi } = require("openai");
+
+class OpenAiClient {
+  constructor(location) {
+    const configuration = new Configuration({
+      apiKey: process.env.OPENAI_API_KEY,
+    });
+    this.openai = new OpenAIApi(configuration);
+    this.prompt = `What are some activities I can do with friends in ${location}? Only give me the list. Make each suggestion less than 10 words. Make output new line seperated without numbers.`;
+  }
+
+  eventSearch(callback) {
+    this.openai
+      .createChatCompletion({
+        model: "gpt-3.5-turbo",
+        messages: [
+          { role: "system", content: "You are a helpful assistant." },
+          {
+            role: "user",
+            content: this.prompt,
+          },
+        ],
+        temperature: 0.0,
+      })
+      .then((data) => {
+        const result = data.data.choices[0].message.content;
+        const activities = result.split("\n").map((activity) => {
+          return activity.replace(/^\d+\.\s/, "");
+        });
+        callback(activities);
+      });
+  }
+}
+
+const callback = (data) => {
+  console.log(data);
+};
+
+const client = new OpenAiClient("London");
+client.eventSearch(callback);

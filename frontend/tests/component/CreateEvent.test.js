86a4ead549b9a23d6ded0d9443ce6de8afddc73a
Evan Thomas <evanthomas0292@gmail.com>
2023-07-08 15:06:49 +0100

    Write skeleton xtests for CreateEvent component and begin writing

--- frontend/tests/component/CreateEvent.test.js
+++ frontend/tests/component/CreateEvent.test.js
@@ -1,21 +1,62 @@
-import { render } from '@testing-library/react-native';
-import { Text } from "react-native"
-import NewEvent from '../../app/new-event';
+import { render, screen } from "@testing-library/react-native";
+import { Text } from "react-native";
+import CreateEvent from "../../components/CreateEvent";
 
 // needs to be created
-describe(NewEvent, () => {
-//   test('renders the correct title', () => {
-//     const choices = []
-//     const { getByText } = render(<MatchResults choices={choices}/>);
-//     const textElement = getByText('Your Matches');
-//     expect(textElement).toBeDefined();
-//   });
+describe(CreateEvent, () => {
+  beforeEach(() => {
+    const name = "testName1";
+    const nameInput = jest.fn();
+    const nameList = ["testName2", "testName3"];
+    const eventName = "";
+    const addName = jest.fn();
+    const removeName = jest.fn();
+    const eventNameInput = jest.fn();
+    render(
+      <CreateEvent
+        name={name}
+        nameInput={nameInput}
+        nameList={nameList}
+        eventName={eventName}
+        addName={addName}
+        removeName={removeName}
+        eventNameInput={eventNameInput}
+      />
+    );
+  });
 
-//   test('Scrollview contains the correct amount of text elements', () => {
-//     const choices = ["GravyBoat", "SalmonHat"]
-//     const { getByTestId } = render(<MatchResults choices={choices}/>);
-//     const item1 = getByTestId("scroll-view-1");
-//     const textElements = item1.findAllByType(Text);
-//     expect(textElements.length).toBe(2);
-//   })
+  test("renders the correct title", () => {
+    const textElement = screen.getByText("Create Event");
+    expect(textElement).toBeDefined();
+  });
+
+  test("starts with an empty event name", () => {
+    const textInputElement = screen.getByPlaceholderText("Enter event name");
+    expect(textInputElement).toHaveTextContent("");
+  });
+
+  xtest("displays the event name when one is provided", () => {});
+
+  xtest("eventNameInput is called with new event name when text changes", () => {});
+
+  test("starts with an empty name", () => {
+    const textInputElement = screen.getByPlaceholderText(
+      "Enter name and press enter..."
+    );
+    expect(textInputElement).toHaveTextContent("");
+  });
+
+  xtest("displays your name when one is provided", () => {});
+
+  xtest("nameInput is called with new name when text changes", () => {});
+
+  xtest("addName is called when friend's name is submitted", () => {});
+
+  xtest("there are no names displayed at first", () => {});
+
+  xtest("diplays friend's names that have been added", () => {});
+
+  xtest("if friend's names are passed in they are displayed", () => {});
+
+  xtest("removeName is called with the name when name is deleted", () => {});
 });

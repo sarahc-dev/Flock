134848642af1d09b9cb04d2b96fa8abaaf837955
Matt Hammond <mjhammond94@gmail.com>
2023-07-10 17:15:45 +0100

    Add useEffect funct - update dropdownOptions

--- frontend/app/event-chooser.js
+++ frontend/app/event-chooser.js
@@ -1,26 +1,46 @@
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View } from "react-native";
 import FlashCardContainer from "../components/FlashCardContainer";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import MatchResults from "../components/MatchResults";
 import { Link } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
 import DropdownMenu from "../components/DropdownMenu";
+import { IP } from "@env";
 
 export default function Home() {
     const [card, setCard] = useState(0);
     const [user, setUser] = useState(1);
     const [results, setResults] = useState(false);
     const [selectedName, setSelectedName] = useState("");
+    const [dropdownOptions, setDropDownOptions] = useState([])
 
     const [choices, setChoices] = useState([]);
     const activities = ["go for a walk", "eat pizza", "dance party", "have a conversation", "base jumping"];
 
-    const dropdownOptions = [
-        { label: "Sarah", value: "Sarah" },
-        { label: "Verity", value: "Verity" },
-        { label: "Evan", value: "Evan" },
-    ];
+
+    useEffect(() => {
+      fetch(`http://${IP}:8080/event/64ac02a6d8f4e024dc9fa59b`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json",
+        }
+        })
+        .then(response => response.json())
+        .then(data => {
+          const updatedOptions = data.names.map(user => ({
+            label: user.name,
+            value: user.name
+        }));
+
+        console.log(updatedOptions)
+            
+          setDropDownOptions(updatedOptions);
+        })
+        .catch(error => {
+          console.error(error);
+        });
+    }, [])
 
     const addChoice = choice => {
         setChoices([...choices, choice]);

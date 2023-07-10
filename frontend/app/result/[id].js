733d5a4cc6cc655ace6b103eebeb1700e2226058
marco belahouane <nfk18@hotmail.co.uk>
2023-07-10 17:56:39 +0100

    waiting functionality

--- frontend/app/result/[id].js
+++ frontend/app/result/[id].js
@@ -6,10 +6,11 @@ import { IP } from "@env"
 import ListItem from "../../components/ListItem";
 export default function Result(props) {
     const { id } = useLocalSearchParams();
-    const [data, setData] = useState()
-    // const { activities} = props
     const activities = ["go for a walk", "eat pizza", "dance party", "have a conversation", "base jumping"];
-
+    const [data, setData] = useState({names: [{name: 'john', choices: ['asda']}, {name: 'jim', choices: ['asda']}], activities })
+    const [isComplete, setIsComplete] = useState(false)
+    // const { activities} = props
+    
     // {_id: udfuadfa, names: [userObject], activities: [], eventName: "something"}
 
     // {name: "Sarah", choices: ["dnfsdf", "dfss"]}
@@ -19,33 +20,53 @@ export default function Result(props) {
     // isComplete? Has each person got an array of choices?
     // either include no choices or a default option if no choices
 
-    useEffect(() => {
-        if (id) {
-            fetch(`http://${IP}:8080/event/${id}`)
-            .then(response => response.json())
-            .then(data => setData(data))
-        }
-    }, [id])
+    // useEffect(() => {
+    //     if (id) {
+    //         fetch(`http://${IP}:8080/event/${id}`)
+    //         .then(response => response.json())
+    //         .then(data => setData(data))
+    //     }
+    // }, [id])
 
     useEffect(() => {
-        // isComplete?
-        data.names.forEach((name) => {
-
+        
+        console.log(data)
+        const filteredData = data.names.filter((user) => {
+           return user.choices.length === 0
         })
+        console.log(filteredData);
+        (filteredData.length === 0) ? setIsComplete(true) : console.log('waiting') 
+          console.log(isComplete)
     }, [data])
 
-    const renderItem = ({item}) => (
-        <ListItem activity={item} />
-    )
        
+    if (!isComplete) {
     return (
+      <SafeAreaView>
+           <Text>waiting</Text>
+          <Text>Dynamic result page </Text> 
+              <Text>{data.eventName}</Text>
+        
+
+        <View>
+            <Link href="/">Go Home</Link>
+        </View>
+        </SafeAreaView>
+    )
+      }
+
+    return (
+  
         <SafeAreaView>
-        <Text>Dynamic result page </Text>
-        {data && <><Text>{data.eventName}</Text>
-        <FlatList
-        data={activities}
-        renderItem={renderItem}
-      /></>}
+           <Text>wait</Text>
+          <Text>Dynamic result page </Text> 
+              <Text>{data.eventName}</Text>
+              <FlatList
+              data={data.activities}
+              renderItem={({item}) => (
+            <ListItem activity={item} />)}
+        />
+        
 
         <View>
             <Link href="/">Go Home</Link>

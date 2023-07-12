c465df607546c0f424ecd33feaf986e6a1710dcf
Evan Thomas <evanthomas0292@gmail.com>
2023-07-12 15:32:41 +0100

    Push some local storage logging in

--- frontend/app/results-page.js
+++ frontend/app/results-page.js
@@ -1,10 +1,35 @@
+import { useState, useEffect } from "react" 
 import { Text } from "react-native" 
 import { SafeAreaView } from "react-native-safe-area-context";
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 
 export default function NewEvent() {
+  const [data, setData] = useState("")
+
+  const getData = async () => {
+    try {
+      const value = await AsyncStorage.getItem('pastEvents');
+      console.log(value)
+      if (value !== null) {
+        // value previously stored
+        const localData = JSON.parse(value)
+        setData(localData);
+      }
+    } catch (e) {
+      // error reading value
+    }
+  };
+
+  useEffect(() => {
+    getData();
+  }, [])
+
+  console.log(`data: ${data}`)
+
   return (
     <SafeAreaView>
-      <Text>Did it</Text>
+      {data && <Text>data[0].eventName</Text>}
     </SafeAreaView>
   )
 }

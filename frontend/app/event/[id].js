160c5b4c6d22b738704d8847b43100c89da9ee9d
Matt Hammond <mjhammond94@gmail.com>
2023-07-12 13:59:00 +0100

    Remove event chooser files

--- frontend/app/event/[id].js
+++ frontend/app/event/[id].js
@@ -1,36 +1,3 @@
-// import { useEffect, useState } from "react";
-// import { useLocalSearchParams } from 'expo-router';
-// import { SafeAreaView, Text, View } from "react-native";
-// import { Link } from "expo-router"
-// import { IP } from "@env"
-// import Header from "../../components/Header";
-
-// export default function Event() {
-//     const { id } = useLocalSearchParams();
-//     const [data, setData] = useState()
-
-//     useEffect(() => {
-//         if (id) {
-//             fetch(`http://${IP}:8080/event/${id}`)
-//             .then(response => response.json())
-//             .then(data => setData(data))
-//         }
-//     }, [id])
-
-//     return (
-//         <SafeAreaView>
-//         <Header name={'Events'} />
-//         <Text>Dynamic event page - {id}</Text>
-//         {data && <Text>{data.eventName}</Text>}
-
-//         <View>
-//             <Link href="/">Go Home</Link>
-//         </View>
-//         </SafeAreaView>
-//     )
-// }
-
-
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, View, TouchableOpacity, Text } from "react-native";
 import FlashCardContainer from "../../components/FlashCardContainer";
